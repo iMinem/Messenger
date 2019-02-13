7b58155b55027af7673432b13c35ecac9fc23f13
Max Marhofen <maximilianmarhofen@maxs-macbook-air.local>
2019-02-13 10:11:26 +0100

    Initial Commit

---
+++ Messenger/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Messenger
+//
+//  Created by Max Marhofen on 13.02.19.
+//  Copyright © 2019 EL2M. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
